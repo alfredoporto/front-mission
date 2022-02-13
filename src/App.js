0c4a2f0e5a0f0088d03d7f53e29a66dae4370a92
Alfredo Portocarrero <benalfred42@gmail.com>
2022-02-13 15:24:34 -0500

    feat 001-component-about

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { About } from './components';
+
 function App() {
   return (
-    <div>
-      Inicio
-    </div>
+    <Router>
+      <div>
+        Navbar
+        <Routes>
+          <Route path="/about" element={< About />} />
+        </Routes>
+      </div>
+
+    </Router>
   );
 }
 
---
+++ src/components/About/About.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+function About() {
+    return (
+        <div className="container mt-4">
+            <div className="jumbotron">
+                <h1 className="display-4">Alfredo Portocarrero</h1>
+
+                <h3>Software Engineer</h3>
+                <p>Social Media</p>
+                <p className="lead">
+                    <a href="http://">Linkedin</a>
+                    <br />
+                    <a href="http://">Instagram</a>
+                    <br />
+                    <a href="http://">FaceBook</a>
+                </p>
+                <hr className="my-4" />
+                <p>
+                    Hi, I'm Alfredo :)
+                </p>
+                <p>email : benalfred42@gmail.com</p>
+            </div>
+        </div>
+    );
+}
+
+export default About;
